9eed8e245b84f2480cc3c02e6193d7b6534ff0d9
AndreyR05 <andrey.rodrigues@sptech.school>
2024-08-21 21:24:43 -0300

    change instance names

--- ec2-2.tf
+++ ec2-2.tf
@@ -2,6 +2,6 @@ resource "aws_instance" "ec2-terraform02" {
     ami = "ami-0e86e20dae9224db8"
     instance_type = "t2.micro"
     tags = {
-        Name = "terraform-ec2-separado"
+        Name = "terraform-ec2-separado-2"
     }
 }
--- main.tf
+++ main.tf
@@ -12,11 +12,3 @@ terraform {
 provider "aws" {
   region = "us-east-1"
 }
-
-resource "aws_instance" "ec2-terraform01" {
-    ami = "ami-0e86e20dae9224db8"
-    instance_type = "t2.micro"
-    tags = {
-        Name = "terraform-ec2"
-    }
-}
